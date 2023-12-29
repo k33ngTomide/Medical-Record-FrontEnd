0e1eaec298797a6a1e57fa2fae783e46e2cce2e3
k33ngTomide <Msodiqakintomide@gmail.com>
2023-12-29 14:03:07 +0100

    Added the profile dummy image

--- src/components/RightDashBoard.js
+++ src/components/RightDashBoard.js
@@ -1,4 +1,4 @@
-import profile from '../logo-used.png'
+import profile from '../assets/profile_dummy.png'
 
 export function RightDashBoard(){
 
@@ -10,22 +10,22 @@ export function RightDashBoard(){
 
       <div className="details-pane">
         <div className="details-info">
-          <h2>Patients</h2>
+          <h1>Patients</h1>
           <h1>23</h1>
         </div>
 
         <div className="details-info">
-          <h2>Hospitals</h2>
+          <h1>Hospitals</h1>
           <h1>23</h1>
         </div>
 
         <div className="details-info">
-          <h2> Medical History</h2>
+          <h1> Medical History</h1>
           <h1>230</h1>
         </div>
 
         <div className="details-info">
-          <h2> Medical History</h2>
+          <h1> Medical History</h1>
           <h1>230</h1>
         </div>
 
@@ -34,15 +34,52 @@ export function RightDashBoard(){
       <div className='profile-pane'>
 
         <img src={profile} alt='profile' />
-        <>
-          <h1>Name: <span>Username</span></h1>
-          <p></p>
+        <button>Change Image</button>
 
-        </>
+        <div>
+          <h1>Name: <span>Username</span></h1>
+          <p>Specialization: <span>Specialization</span></p>
+          <p>License: <span>License</span></p>
+          <p>Status: <span>Active</span></p>
+        </div>
 
       </div>
 
-      <div className='patients'>
+      <div className='patients' id='patients'>
+        <button id='add-patient'>Add Patients</button>
+      </div>
+
+      <div className='hospitals' id='hospital'>
+        <button id='add-hospital'>Add hospitals</button>
+      </div>
+
+      <div className='helps'>
+        <h2>Welcome to Standard Med Support center</h2>
+
+        <div className='report' id='report-pane'>
+          <h4>Report your issues here</h4>
+          <select>
+            <option>Select</option>
+            <option>Installation and Account Setup</option>
+            <option>Navigating the App</option>
+            <option>Error Messages</option>
+            <option>Feedback and Suggestions</option>
+            <option>Need Technical Support</option>
+            <option>Feature Requests</option>
+            <option>Common Issues</option>
+          </select>
+
+          <textarea rows={10} cols={40} />
+          <button>Submit</button>
+
+        </div>
+
+
+        <p>Follow Us:
+          Stay connected with us on social media for updates, tips, and community highlights.
+          Subscribe to our newsletter for the latest news and exclusive content.
+          Thank you for choosing Standard Med! We are here to ensure you have a seamless experience. If you can't find the information you need, don't hesitate to reach out to our support team. Your satisfaction is our priority.
+        </p>
 
       </div>
     </div>
